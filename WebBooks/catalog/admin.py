a0906197c918068a46bafeeeb38ca930da810cb2
frqhero <frqhero@gmail.com>
2022-05-05 23:42:37 +0300

    fruits of recent work (8 chapter, forms customizing in progress)

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -2,9 +2,22 @@ from django.contrib import admin
 from .models import Author, Book, Genre, Language, Status, BookInstance
 
 # Register your models here.
-admin.site.register(Author)
-admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(Book)
 admin.site.register(Genre)
 admin.site.register(Language)
 admin.site.register(Status)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+admin.site.register(Author, AuthorAdmin)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'language', 'display_author')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
